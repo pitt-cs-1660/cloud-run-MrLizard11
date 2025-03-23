997fcef57cbec67c295d0e03a1111da631022939
Jamie A <jaa230@pitt.edu>
2025-03-23 13:31:21 -0400

    done

--- static/config.js
+++ static/config.js
@@ -6,12 +6,6 @@
 /**
  * ++++ ADD YOUR FIREBASE CONFIGURATION BELOW ++++
  * === Firebase Apikey and Domain ===
- * client_id 483645036888-87caun4k7urvbdgh9fq76n2dega4s513.apps.googleusercontent.com
- * Client secret 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
- *   projectId: "cloud-run-454522",
-  storageBucket: "cloud-run-454522.firebasestorage.app",
-  messagingSenderId: "945863103277",
-  appId: "1:945863103277:web:604a788e0da7e216d839a4"
  */
 
 /**
